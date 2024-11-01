79900702f53e56de2c01436c519487014634c721
RichardWagenknecht <richardwagenknecht@icloud.com>
2024-11-01 17:45:14 -0300

    Declaração Variaveis e Metodos Update e Start

--- TDR/Assets/Inimigos/InimigoBase.cs
+++ TDR/Assets/Inimigos/InimigoBase.cs
@@ -1,18 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class InimigoBase : MonoBehaviour
+public class InimigoBase : MonoBehaviour, IReceberDano
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected float speed = 3f; // Velocidade padrão
+    [SerializeField] protected int hitPoints = 2; // Pontos de vida
+    [SerializeField] protected int currencyWorth = 50; // Valor em moeda ao ser destruído
+
+    protected Rigidbody2D rigidbody;
+    protected Transform target;
+    protected int pontoIndex = 0;
+    private MovimentoInimigo movimentoInimigo;
+
+    public virtual void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        target = GameManager.main.pontos[pontoIndex];
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
-        
+        // Verifica se o inimigo chegou ao destino
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
+            pontoIndex++;
+
+            if (pontoIndex == GameManager.main.pontos.Length)
+            {
+                GameManager.onEnemyDestroy.Invoke();
+                // Remove o inimigo da lista de inimigos ativos
+                GameManager.main.RemoveEnemyFromList(gameObject);
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = GameManager.main.pontos[pontoIndex];
+            }
+        }
     }
+
 }
