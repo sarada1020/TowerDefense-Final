e9770025ccafe374238049b6ea7aaff76a19627f
RichardWagenknecht <richardwagenknecht@icloud.com>
2024-11-01 17:54:27 -0300

    GameManager Scirpt Declaração Variaveis

---
+++ TDR/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager main; // Instância única do GerenciadorJogo
+
+    [Header("Configurações de Spawn de Inimigos")]
+    [SerializeField] List<GameObject> prefabInimigo; // Lista de prefabs de inimigos
+    [SerializeField] int inimigosBase = 8; // Número base de inimigos por onda
+    [SerializeField] float inimigosPorSegundo = 0.5f; // Taxa de spawn de inimigos
+    [SerializeField] float tempoEntreOndas = 5f; // Tempo entre ondas de inimigos
+    [SerializeField] float dificuldadeEscalonamento = 0.75f; // Escalonamento da dificuldade
+
+    [Header("Configurações de Pontos")]
+    public Transform pontoInicial; // Ponto de spawn inicial dos inimigos
+    public Transform[] pontos; // Array de pontos de spawn
+
+    [Header("Configurações de Moeda")]
+    public int currency = 100; // Quantidade inicial de moedas
+
+    int ondaAtual = 1; // Onda atual de inimigos
+    float tempoDesdeUltimoSpawn; // Tempo desde o último spawn de inimigos
+    int inimigosVivos; // Número de inimigos vivos
+    int inimigosRestantesParaSpawn; // Inimigos restantes para spawnar
+    bool isSpawning = false; // Estado do spawn
+
+    public static UnityEvent onEnemyDestroy = new UnityEvent(); // Evento acionado ao destruir um inimigo
+    private List<GameObject> inimigosAtivos = new List<GameObject>(); // Lista para armazenar inimigos ativos
+
+    
+}
