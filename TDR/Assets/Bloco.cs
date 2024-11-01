94029801522c7ca053d0ab4870a8766a4c90fbd7
RichardWagenknecht <richardwagenknecht@icloud.com>
2024-11-01 17:57:59 -0300

    Metodos OnMouse

--- TDR/Assets/Bloco.cs
+++ TDR/Assets/Bloco.cs
@@ -15,4 +15,29 @@ public class Bloco : MonoBehaviour
         startColor = spriteRenderer.color; // Armazena a cor original
     }
 
+    private void OnMouseEnter()
+    {
+        spriteRenderer.color = hoverColor; // Muda a cor do bloco ao passar o mouse
+    }
+
+    private void OnMouseExit()
+    {
+        spriteRenderer.color = startColor; // Restaura a cor original ao sair com o mouse
+    }
+
+    private void OnMouseDown()
+    {
+        if (tower != null) return; // Retorna se já houver uma torre construída
+
+        Torre towerToBuild = GerenciadorConstrução.main.GetSelectedTower(); // Obtém a torre selecionada
+
+        if (towerToBuild.custo > GameManager.main.currency) // Verifica se há moedas suficientes
+        {
+            Debug.Log("Você não pode comprar esta torre."); // Log de erro se não houver moedas suficientes
+            return;
+        }
+        GameManager.main.GastarMoeda(towerToBuild.custo); // Deduz o custo da torre
+
+        tower = Instantiate(towerToBuild.prefab, transform.position, Quaternion.identity); // Instancia a nova torre
+    }
 }
