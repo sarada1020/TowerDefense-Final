9644bb721faf0818f558d2710d1a5341c11a3e90
RichardWagenknecht <richardwagenknecht@icloud.com>
2024-11-01 17:58:58 -0300

    Interface IReceberDano

---
+++ TDR/Assets/IReceberDano.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+// Interface que define o comportamento de objetos que podem receber dano
+public interface IReceberDano
+{
+    void TakeDamage(int dano); // Método para receber dano
+}
